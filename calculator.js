c51664101445f970fe50b69aada357803642a90b
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-05-29 00:56:14 +0800

    added in body-parser and created basic calculation function

--- calculator.js
+++ calculator.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function (req, res) {
-  console.log("server 3000 is running")
+app.post('/', function (req, res) {
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+
+  var result = num1 + num2
+
+  res.send('Your calculate number is ' + result + " !");
 });
 
-var x = "twenty"
+app.listen(3000, function (req, res) {
+  console.log('server 3000 is running');
+});
