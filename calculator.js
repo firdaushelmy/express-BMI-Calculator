799c1e1528b5952f80ae7d13d17db55efbe7dc18
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-05-29 17:54:03 +0800

    added in functionality to bmi, enabling user to choose weight and height unit of measurement

--- calculator.js
+++ calculator.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// index
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
@@ -16,6 +17,53 @@ app.post('/', function (req, res) {
 
   res.send('Your calculate number is ' + result + " !");
 });
+
+// bmi
+app.get('/bmi', function (req, res) {
+  res.sendFile(__dirname + '/bmiCalculator.html');
+});
+
+app.post('/bmi', function (req, res) {
+  var weightCalculate = Number(req.body.weight)
+  var weightUnit = req.body.weightMeasurement
+  var heightCalculate = Number(req.body.height)
+  var heightUnit = req.body.heightMeasurement
+
+  if (weightUnit === 'kg' && heightUnit === 'cm') {
+    var cmToM2Conversion = heightCalculate / 100
+    heightCalculate = cmToM2Conversion * cmToM2Conversion
+    var resultKgCm = weightCalculate / heightCalculate
+
+    res.send("Your BMI is " + resultKgCm + "")
+  }
+
+  else if (weightUnit === 'kg' && heightUnit === 'ft') {
+    var cmToM2Conversion = heightCalculate / 3.281
+    heightCalculate = cmToM2Conversion * cmToM2Conversion
+    var resultKgCm = weightCalculate / heightCalculate
+
+    res.send("Your BMI is " + resultKgCm + "")
+  }
+
+  else if (weightUnit === 'lb' && heightUnit === 'cm') {
+    var cmToM2Conversion = heightCalculate / 100
+    heightCalculate = cmToM2Conversion * cmToM2Conversion
+    var lbToKg = weightCalculate / 2.205
+    var resultKgCm = lbToKg / heightCalculate
+
+    res.send("Your BMI is " + resultKgCm + "")
+  }
+
+  else if (weightUnit === 'lb' && heightUnit === 'ft') {
+    var cmToM2Conversion = heightCalculate / 3.281
+    heightCalculate = cmToM2Conversion * cmToM2Conversion
+    var lbToKg = weightCalculate / 2.205
+    var resultKgCm = lbToKg / heightCalculate
+
+    res.send("Your BMI is " + resultKgCm + "")
+  }
+})
+
 
 app.listen(3000, function (req, res) {
   console.log('server 3000 is running');
