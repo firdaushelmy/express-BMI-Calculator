3c39ef2dce92d336b309e7fb389cee9d12abeabb
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-05-30 00:10:14 +0800

    cleaned up sandbox code and added in readme

--- calculator.js
+++ calculator.js
@@ -4,26 +4,12 @@ const bodyParser = require('body-parser')
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// index
+// bmi
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/', function (req, res) {
-  var num1 = Number(req.body.num1);
-  var num2 = Number(req.body.num2);
-
-  var result = num1 + num2
-
-  res.send('Your calculate number is ' + result + " !");
-});
-
-// bmi
-app.get('/bmi', function (req, res) {
-  res.sendFile(__dirname + '/bmiCalculator.html');
-});
-
-app.post('/bmi', function (req, res) {
   var weightCalculate = Number(req.body.weight)
   var weightUnit = req.body.weightMeasurement
   var heightCalculate = Number(req.body.height)
